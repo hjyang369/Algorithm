dc49c455c1cabfe1c765486d42e0c0cdd12dcf6d
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-01-16 11:07:32 +0900

    [Silver II] Title: 쇠막대기, Time: 244 ms, Memory: 12252 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "10799.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+let stack = [];
+let answer = 0;
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "(" && input[i + 1] === ")") {
+    answer += stack.length;
+  } else if (input[i] === "(" && input[i + 1] !== ")") {
+    stack.push(input[i]);
+  } else if (input[i - 1] !== "(" && input[i] === ")") {
+    stack.pop();
+    answer += 1;
+  }
+}
+
+console.log(answer)
