f0316427ad0ef4b73b78e4527a4378fdf0c47f1c
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-02-22 00:06:26 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const n = Number(input);
+const dp = Array(n).fill(0);
+dp[1] = 1;
+
+for (let i = 1; i <= n; i++) {
+  dp[i + 1] = BigInt(dp[i]) + BigInt(dp[i - 1]);
+}
+
+console.log(dp[n].toString());
