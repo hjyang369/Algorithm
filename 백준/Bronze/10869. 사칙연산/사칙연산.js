f8814a5a13a54a3c8f3f00e932a508b266e809e8
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-07-21 23:07:15 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(input[0] + input[1]);
+console.log(input[0] - input[1]);
+console.log(input[0] * input[1]);
+console.log(parseInt(input[0] / input[1]));
+console.log(input[0] % input[1]);
