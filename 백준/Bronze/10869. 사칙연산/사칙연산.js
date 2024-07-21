f109063f3032a442d0e0ffeb2239d2a4f631eb4f
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-07-21 23:08:00 +0900

    Update 사칙연산.js

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -12,3 +12,5 @@ console.log(input[0] - input[1]);
 console.log(input[0] * input[1]);
 console.log(parseInt(input[0] / input[1]));
 console.log(input[0] % input[1]);
+
+// 몫을 구하기 위해 나눗셈 한 후 정수만 남겨주는 방식으로 parseInt 사용
