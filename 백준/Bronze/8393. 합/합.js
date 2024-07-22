034ed7f1043894647d6ef91aeed828d6f4b0915f
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-07-22 19:57:22 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString();
+
+let result = 0;
+for (let i = 1; i <= input; i++) {
+  result += i;
+}
+
+console.log(result);
