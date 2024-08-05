3d6e159e5dd1376aef9ec1c326d098afedc2efe2
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-05 21:35:34 +0900

    Update 주사위 세개.js

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -15,3 +15,7 @@ if (A !== B && B !== C && C !== A) {
 } else {
   console.log(1000 + B * 100);
 }
+
+// 다른 사람 풀이
+// else console.log(Math.max(a,b,c)*100); -> max로 최댓 값 가져옴
+// 나는 이미 정렬이 되어 있어서 B는 무조건 같은 값이라서 B로 계산했음
