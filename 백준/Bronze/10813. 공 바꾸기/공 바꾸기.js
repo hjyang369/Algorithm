fcef75b40b005a24b71e8c4955b5f9f763f825c6
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-13 23:24:05 +0900

    Update 공 바꾸기.js

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.js
@@ -18,3 +18,21 @@ for (let k = 0; k < M; k++) {
 }
 
 console.log(pocket.join(" "));
+
+
+// 다른 사람 풀이
+// const fs = require('fs');
+// const input = fs.readFileSync(0, 'utf-8')
+
+// 전개연산자로 푸는 방법
+// const [[N, M], ...cases] = input.trim().split("\n").map(line => line.split(" ").map(Number));
+
+// const arr = new Array(N).fill(0).map((_, i) => i + 1);
+
+// 나는 따로 배열을 복사해서 값을 바꿔줘야만 값을 유지하면서 바꿀 수 있었는데
+// 한번에 바꿔버리니 굳이 신경 쓸 필요가 없어서 좋은듯
+// cases.forEach(([i, j]) => {
+//     [arr[i - 1], arr[j - 1]] = [arr[j - 1], arr[i - 1]];
+// })
+
+// console.log(arr.join(" "));
