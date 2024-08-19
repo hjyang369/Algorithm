0aebf956a643a574a11dc4f46a010bc65b9b041e
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-19 23:23:38 +0900

    [Bronze II] Title: 나머지, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((ele) => Number(ele) % 42);
+
+console.log([...new Set(input)].length);
