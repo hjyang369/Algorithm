9e7fb542e794ce8ebc655ffcdacee496c9ca20dc
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-08-22 22:27:24 +0900

    Update 과제 안 내신 분．．？.js

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -8,3 +8,11 @@ const input = fs
   .map(Number);
 const arr = Array.from({ length: 30 }, (_, i) => i + 1);
 console.log(arr.filter((ele) => !input.includes(ele)).join("\n"));
+
+// 다른사람 풀이 
+// 항상 indexOf를 생각해내지 못한다ㅜ
+// for(var i = 1; i <= 30; ++i) {
+//   if(n.indexOf(i.toString()) === -1) {
+//     console.log(i);
+//   }
+// }
