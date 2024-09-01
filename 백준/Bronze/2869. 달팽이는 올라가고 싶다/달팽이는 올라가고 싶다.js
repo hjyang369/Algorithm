3fdc3ce95b18415fdea5270217e142df3bfdd59c
hjyang369 <125977702+hjyang369@users.noreply.github.com>
2024-09-01 23:07:11 +0900

    Update 달팽이는 올라가고 싶다.js

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -8,3 +8,21 @@ const [A, B, V] = fs
   .map(Number);
 
 console.log(Math.ceil((V - B) / (A - B)));
+
+// 반복문을 돌면 시간초과가 나왔다.
+// 아래처럼 풀면 답이 나오긴 하지만 반복문이 필요없는 문제였다.
+// let day = 0;
+// let result = 0;
+
+// while (V > result) {
+//   result = result + A;
+//   day += 1;
+
+//   if (result >= V) {
+//     break;
+//   }
+
+//   result = result - B;
+// }
+
+// console.log(day);
